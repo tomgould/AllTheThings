ddc8cd3bd1057e9df39c56bcaad245252ecb6421
tom.gould <tom@dealmobile.co.uk>
2016-02-12 17:58:02 +0000

    Some changes to make it better

--- AllTheThings.php
+++ AllTheThings.php
@@ -1,29 +1,41 @@
 <?php
+/**
+ * @file
+ *     Zips up the $dir_to_zip andcreates a zip at $zip_file_name,
+ *     sends it to the browser and deletes the zip from the server
+ */
+// Replace these values
+$dir_to_zip    = '/home/tgould/www/ines/';
+$zip_file_name = '/tmp/ALLTHETHINGS.zip';
+
+all_the_things($dir_to_zip, $zip_file_name);
+unlink($zip_file_name);
 
 /**
  * Zips all the things and buffers the zip to your browser
  *
- * @param string $source
+ * @param string $dir_to_zip
  *   The dir to zip
- * @param type $destination
+ * @param type $zip_file_name
  *   The temporary writable location eg "/tmp/ALLTHETHINGS.zip"
  * 
  */
-function all_the_things($source, $destination) {
+function all_the_things($dir_to_zip, $zip_file_name) {
   ini_set('memory_limit', -1);
-  if (!extension_loaded('zip') || !file_exists($source)) {
+  set_time_limit(600);
+  if (!extension_loaded('zip') || !file_exists($dir_to_zip)) {
     return false;
   }
 
   $zip = new ZipArchive();
-  if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+  if (!$zip->open($zip_file_name, ZIPARCHIVE::CREATE)) {
     return false;
   }
 
-  $source = str_replace('\\', '/', realpath($source));
+  $dir_to_zip = str_replace('\\', '/', realpath($dir_to_zip));
 
-  if (is_dir($source) === true) {
-    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+  if (is_dir($dir_to_zip) === true) {
+    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir_to_zip), RecursiveIteratorIterator::SELF_FIRST);
 
     foreach ($files as $file) {
       $file = str_replace('\\', '/', $file);
@@ -35,29 +47,26 @@ function all_the_things($source, $destination) {
       $file = realpath($file);
 
       if (is_dir($file) === true) {
-        $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+        $zip->addEmptyDir(str_replace($dir_to_zip . '/', '', $file . '/'));
       }
       else if (is_file($file) === true) {
-        $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+        $zip->addFromString(str_replace($dir_to_zip . '/', '', $file), file_get_contents($file));
       }
     }
   }
-  else if (is_file($source) === true) {
-    $zip->addFromString(basename($source), file_get_contents($source));
+  else if (is_file($dir_to_zip) === true) {
+    $zip->addFromString(basename($dir_to_zip), file_get_contents($dir_to_zip));
   }
 
   $zip->close();
 
   header("Content-type: application/zip");
-  header("Content-Disposition: attachment; filename=" . basename($destination) . '.zip');
-  header("Content-length: " . filesize($destination));
+  header("Content-Disposition: attachment; filename=" . basename($zip_file_name));
+  header("Content-length: " . filesize($zip_file_name));
   header("Pragma: no-cache");
   header("Expires: 0");
-  readfile("$destination");
+  readfile("$zip_file_name");
 
   exit;
 
 }
-
-all_the_things('/var/www/sitename/docroot/', '/tmp/ALLTHETHINGS.zip');
-
